50faf1b7c669ebecdd5a2cb4bcd37a6278f449a9
maximsedyar <maksym@doubledmarketing.com>
2021-06-22 13:07:38 +0300

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError();
+  }
+
+  return a + b;
+}
+
+function isNumber(value) {
+  return typeof value === 'number';
 }
 
 module.exports = sum;
