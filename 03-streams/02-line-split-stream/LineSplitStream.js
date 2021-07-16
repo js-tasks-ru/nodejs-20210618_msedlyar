01e50135d8c4c2796ad3712995e4fc5425e12dca
maximsedyar <maksym@doubledmarketing.com>
2021-07-16 16:45:03 +0300

    task 3/2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,49 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.arr = [];
+    this.limit = 0;
+    this.firstChunk = '';
   }
 
   _transform(chunk, encoding, callback) {
+    let str = Buffer.from(chunk).toString();
+    // let isEOL = str.search(os.EOL);
+
+    // if (isEOL !== -1) {
+      this.arr = str.split(os.EOL);
+      this.limit = this.arr.length;
+    // } else {
+      if (this.firstChunk !== '') {
+        let c = this.firstChunk + chunk;
+        callback(null, c);
+        return;
+      }
+      // this.firstChunk = str;
+      // callback();
+      // return;
+    // }
+
+    this.arr.forEach((chunk, index) => {
+      let c = chunk;
+      if (this.firstChunk !== '') {
+        c = this.firstChunk + chunk;
+        this.firstChunk = '';
+      }
+
+      // if ((this.limit - 1) === index) {
+      //   this.firstChunk = c;
+      // } else {
+        this.push(c);
+      // }
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    callback();
   }
 }
 
---
+++ 03-streams/02-line-split-stream/index.js
@@ -0,0 +1,19 @@
+const LineSplitStream = require('./LineSplitStream');
+const os = require('os');
+
+const lines = new LineSplitStream({
+  encoding: 'utf-8',
+});
+
+let i = 0;
+function onData(line) {
+  console.log(line, 'line - ' + i++);
+}
+
+lines.on('data', onData);
+
+lines.write(`0 строка`);
+lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка`);
+lines.write(`4 строка`);
+// lines.write(`a${os.EOL}b`);
+lines.end();
